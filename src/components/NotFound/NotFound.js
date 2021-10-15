1d84d1f0934706b3146205bab0ed7247ab4a8f41
FarukShovo <farukshovo.yzu.comp@gmail.com>
2021-10-15 17:17:12 +0600

    final

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div className="notFound">
+            <h2>404</h2>
+            <h4>The page you search is not correct</h4>
+        </div>
+    );
+};
+
+export default NotFound;
